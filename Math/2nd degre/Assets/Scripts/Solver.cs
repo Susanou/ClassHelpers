2880044b50712e90267cbaa97d40b2a90af18fa8
Fukurou <caos2804@gmail.com>
2017-11-26 18:06:08 -0500

    added solver

---
+++ Math/2nd degre/Assets/Scripts/Solver.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Solver : MonoBehaviour
+{
+
+    public InputField Va, Vb, Vc; //input values of a, b and c factors of the equation
+    public Text sortie; //text element to prompt for the user
+    private float a, b, c; //coefficient values
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a = (float)double.Parse(Va.text);
+        b = float.Parse(Vb.text);
+        c = float.Parse(Vc.text);
+        sortie.text = EqSolver(a, b, c);
+    }
+
+    /// <summary>
+    /// Solver method
+    /// </summary>
+    /// <param name="a">value of the coefficient of x^2</param>
+    /// <param name="b">value of the coeeficient on x</param>
+    /// <param name="c">value of the last coefficient</param>
+    /// <returns>returns a sign table of the function</returns>
+    string EqSolver(float a, float b, float c)
+    {
+        StringBuilder tableau = new StringBuilder();
+        float disc, disc2;
+        float x1, x2;
+
+        disc = Mathf.Pow(b, 2) - 4 * a * c;
+
+        if (disc > 0)
+        {
+            disc2 = Mathf.Sqrt(disc);
+            x1 = (-b - disc2) / (2 * a);
+            x2 = (-b + disc2) / (2 * a);
+
+            if (x1 > 0 && x2 > 0)
+            {
+                if (a > 0)
+                {
+                    tableau.Append(string.Format(" x                 | -inf           {0:E}             {1:E}            +inf\n" +
+                                                  "___________________|_________________|__________________|________________\n" +
+                                                  "(x-{0:E})(x-{1:E}) |        +        0         -        0        +       \n"));
+                }
+                else
+                {
+                    tableau.Append(string.Format(" x                 | -inf           {0:E}             {1:E}            +inf\n" +
+                                                 "___________________|_________________|__________________|________________\n" +
+                                                 "(x-{0:E})(x-{1:E}) |        -        0         +        0        -       \n"));
+                }
+            }
+        }
+
+
+        return tableau.ToString();
+    }
+}
